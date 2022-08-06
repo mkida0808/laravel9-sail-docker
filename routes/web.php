fd8cf7167229a266275f3ded5d52322a1fa76919
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-07 00:16:49 +0900

    テストデータをcontrollerに移行、ルーティングの表記方法を調整

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = [
-        'Title A',
-        'Title B',
-        'Title C',
-    ];
-
-    // return view('welcome');
-    return view('index')->with(['posts' => $posts]);
-});
+// Route::get('/', ['App\Http\Controllers\PostController', 'index']);
+// Route::get('/', [App\Http\Controllers\PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
