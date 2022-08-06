eac770ad7de9078bae47aef83d18722bf60caccd
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-07 02:03:45 +0900

    コンポーネントで画面側の共通化bladeを作成、値の受け渡し実装

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-</head>
-<body>
-    <div class="container">
-        <h1>My BBS</h1>
-        <ul>
-            {{-- 配列要素がからである場合を考慮する --}}
-            @forelse ($posts as $index => $post)
-                <li><a href="{{ route('posts.show', $index) }}">{{ $post }}</a></li>
-            @empty
-                <li>投稿データはありません</li>
-            @endforelse
-        </ul>
-    </div>
-</body>
-</html>
+<x-layout>
+    <x-slot name="title">
+        MY BBS
+    </x-slot>
+    <h1>My BBS</h1>
+    <ul>
+        {{-- 配列要素がからである場合を考慮する --}}
+        @forelse ($posts as $index => $post)
+            <li><a href="{{ route('posts.show', $index) }}">{{ $post }}</a></li>
+        @empty
+            <li>投稿データはありません</li>
+        @endforelse
+    </ul>
+</x-layout>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-</head>
-<body>
-    <div class="container">
-        <div class="back-link">
-            &laquo; <a href="{{ route('posts.index') }}">Back</a>
-        </div>
-        <h1>{{ $post }}</h1>
+<x-layout>
+    <x-slot name="title">
+        {{ $post }} - My BBS
+    </x-slot>
+    <div class="back-link">
+        &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
-</body>
-</html>
+    <h1>{{ $post }}</h1>
+</x-layout>
