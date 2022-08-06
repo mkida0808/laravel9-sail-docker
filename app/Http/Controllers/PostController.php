6080c60fd4960721af5e97a3506238bdd09fc53c
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-07 05:43:39 +0900

    コントローラーのテストデータを削除してDBからデータ取得する

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,27 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    private $posts = [
-        'Title A',
-        'Title B',
-        'Title C',
-        'Title D',
-        'Title E',
-        'Title F',
-        'Title G',
-        'Title H',
-        'Title I',
-    ];
     public function index()
     {
-        return view('index')->with(['posts' => $this->posts]);
+        // postsテーブルのデータを全て取得
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+        return view('index')->with(['posts' => $posts]);
     }
 
     public function show($id)
     {
-        return view('posts.show')->with(['post' => $this->posts[$id]]);
+        $post = Post::findOrFail($id);
+        return view('posts.show')->with(['post' => $post]);
     }
 }
