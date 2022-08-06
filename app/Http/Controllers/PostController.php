cd858ec7fa4a5227cc99b06292d5941108c92581
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-07 00:43:00 +0900

    テストデータをプロパティ変数として移行。記事詳細ページshowメソッドへの値渡しを定義

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,19 +6,24 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    private $posts = [
+        'Title A',
+        'Title B',
+        'Title C',
+        'Title D',
+        'Title E',
+        'Title F',
+        'Title G',
+        'Title H',
+        'Title I',
+    ];
     public function index()
     {
-        $posts = [
-            'Title A',
-            'Title B',
-            'Title C',
-            'Title D',
-            'Title E',
-            'Title F',
-            'Title G',
-            'Title H',
-            'Title I',
-        ];
-        return view('index')->with(['posts' => $posts]);
+        return view('index')->with(['posts' => $this->posts]);
+    }
+
+    public function show($id)
+    {
+        return view('posts.show')->with(['post' => $this->posts[$id]]);
     }
 }
