c251291fe946017bcee7644f5fbe7f2b9c2af2fb
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-07 00:45:13 +0900

    記事詳細ページのshow.bladeを新規作成

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="back-link">
+            &laquo; <a href="/">Back</a>
+        </div>
+        <h1>{{ $post }}</h1>
+    </div>
+</body>
+</html>
