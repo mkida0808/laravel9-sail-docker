8a985607a92a494941daa34959be1fc8088eca50
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-07 16:52:54 +0900

    新規投稿ページのbladeファイルを新規作成、それに伴うCSSスタイリング作成

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,4 +6,23 @@
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
     <h1>Add New Post</h1>
+
+    <form method="post" action="{{ route('posts.store') }}">
+        @csrf
+        <div class="form-group">
+            <label>
+                Title
+                <input type="text" name="title">
+            </label>
+        </div>
+        <div class="form-group">
+            <label>
+                Body
+                <textarea name="body"></textarea>
+            </label>
+        </div>
+        <div class="form-button">
+            <button>Add</button>
+        </div>
+    </form>
 </x-layout>
