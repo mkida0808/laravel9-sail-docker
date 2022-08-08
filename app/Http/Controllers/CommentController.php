83f1e72765ce5cac62fab1c5a3d774233c7df43f
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-08 22:30:51 +0900

    コントローラー内で記事詳細ページのコメント削除の処理を実装

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -18,4 +18,11 @@ class CommentController extends Controller
 
         return redirect()->route('posts.show', $post);
     }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return redirect()->route('posts.show', $comment->post);
+    }
 }
