15fb74624a973970b931e34f2e7d58928f2e51e5
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-08 21:58:15 +0900

    投稿に対するコメント登録処理を実装

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Http\Requests\CommentRequest;
+
+class CommentController extends Controller
+{
+    public function store(CommentRequest $request, Post $post)
+    {
+        $comment = new Comment();
+        $comment->post_id = $post->id;
+        $comment->body = $request->body;
+        $comment->save();
+
+        return redirect()->route('posts.show', $post);
+    }
+}
